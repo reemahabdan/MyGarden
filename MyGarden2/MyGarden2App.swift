e67f2979fd226866730f9eb82f4a304e78fe4028
Reema <r.alhabdan@live.com>
2023-01-09 19:31:56 +0300

    Initial Commit

---
+++ MyGarden2/MyGarden2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyGarden2App.swift
+//  MyGarden2
+//
+//  Created by Reema Alhabdan on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyGarden2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
